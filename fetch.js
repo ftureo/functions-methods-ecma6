9980c11f7463a886f902834e2a2441b9af302948
Fabian Tureo Avalith <fabianestebantureo@gmail.com>
2022-08-03 10:57:45 -0300

    :sparkles: feat: add Painting the DOM with MAP and MAP+Destructuring

--- fetch.js
+++ fetch.js
@@ -1,5 +1,3 @@
-console.log("Fetch");
-
 // Fetch con .then()
 // Fetch con async/await
 // Axios async/await
@@ -10,14 +8,40 @@ fetch(URL_USERS) // Infiere en que el pedido es un GET
     .then((response) => response.json())
     .then((data) => showData(data)); //Nested Promises
 
-function showData(data) {
-    console.log("Data", data);
-    let body = ``;
-    for (let i = 0; i < data.length; i++) {
-        // console.log(data[i]);
-        body += `
-            <h2>${data[i].name}</h2>
-        `;
-    }
-    document.getElementById("fetch-petition").innerHTML = body;
-}
+// // Paint the data in the DOM with FOR
+// function showData(data) {
+//     console.log("Data", data);
+//     let body = ``;
+//     for (let i = 0; i < data.length; i++) {
+//         // console.log(data[i]);
+//         body += `
+//             <h2>${data[i].name}</h2>
+//         `;
+//     }
+//     document.getElementById("fetch-petition").innerHTML = body;
+// }
+
+// // Paint the data in the DOM with MAP and Arrow Function Format
+// const showData = (data) => {
+//     console.log("Data", data);
+//     let body = ``;
+//     data.map((user) => {
+//         return (body += `
+//                 <h2>Username: ${user.name}</h2>
+//             `);
+//     });
+//     document.getElementById("fetch-petition").innerHTML = body;
+// };
+
+// // Paint the data in the DOM with MAP and Arrow Function Format + Destructuring
+// const showData = (data) => {
+//     console.log("Data", data);
+//     let body = ``;
+//     data.map(({ name, email }) => {
+//         return (body += `
+//                 <h2>Username: ${name}</h2>
+//                 <h2>Email: ${email}</h2>
+//             `);
+//     });
+//     document.getElementById("fetch-petition").innerHTML = body;
+// };
